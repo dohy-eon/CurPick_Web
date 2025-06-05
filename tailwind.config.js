bc04528d3fb3d83958e7baa57ea5460760dcbca1
HYEON <titeotty12@gmail.com>
2025-06-05 16:22:10 +0900

    feat: Header 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'main': ['Ttangsbudaejjigae', 'sans-serif'],
+        'logo': ['GiazaStencil', 'serif'],
+        'luxgom': ['THELuxGoM', 'sans-serif'],
+      },
+      colors: {
+        'curpick-brown': '#7C4D25', // 로고 관련 색상
+        'text-gray': '#636363', // 로그인/회원가입 텍스트 색상
+      },
+    },
   },
   plugins: [],
 }
